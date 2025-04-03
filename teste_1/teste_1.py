1c3dd60a4e0e5e80129ef2cd6cd482ffcb1545ba
VictorECarvalho <victorelitt@gmail.com>
2025-04-02 23:56:32 -0300

    fix

--- teste_1/teste_1.py
+++ teste_1/teste_1.py
@@ -9,9 +9,10 @@ import shutil
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 ZIP_NAME = os.path.join(DIR_PATH, "teste_1.zip")
 OUTPUT_DIR = os.path.join(DIR_PATH, "temp")
+LINK = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos"
 
-def scrape(regex_pattern):
-    page = requests.get("https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos")
+def scrape(link, regex_pattern):
+    page = requests.get(link)
     soup = BeautifulSoup(page.text, "html.parser")
     pdf_link_list = soup.find_all("a", attrs={"class": "internal-link"})
 
@@ -52,7 +53,7 @@ def delete_temp(folder_name):
 
 if __name__ == "__main__":
     print("Starting the scraping process...")
-    scrape(r"(Anexo_I|Anexo_II).+(\.pdf)")
+    scrape(LINK, r"(Anexo_I|Anexo_II).+(\.pdf)")
     zip_files(OUTPUT_DIR, ZIP_NAME)
     print(f"Files downloaded and zipped into {ZIP_NAME}")
     delete_temp(OUTPUT_DIR)
